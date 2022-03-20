eb97ca0bd59c0f6fddc76a9b949b0599e5ddeea7
Brycen Medart <treantlabs@gmail.com>
2022-03-20 15:56:45 -0700

    Add imports for form components and basic handlers

--- src/App.js
+++ src/App.js
@@ -2,47 +2,62 @@
 import { useState } from "react";
 import InputComponent from "./components/form/InputComponent";
 import FormComponent from "./components/form/FormComponent";
+import ButtonComponent from "./components/form/ButtonComponent";
 
 function App() {
 
   const [form, setForm] = useState({
-    email:'',
-    password:''
+    email: '',
+    password: ''
   });
 
-  const handleFormChange = (e) => {
-    // Get the name of the field that caused event
-    // Get the new value of this field
+  const handleInputChange = (e) => {
+    // Get the name of the field that caused event and the new value of this field
     const { name, value } = e.target;
 
-    // Assign new value to the form field
+    // Assign new value to the form state
     const updatedForm = {
       ...form,
       [name]: value
     };
 
-    console.log('Form changed: ', updatedForm);
-
     // Update state
     setForm(updatedForm);
   }
 
+  const formSubmit = (e) => {
+
+    // Prevent default behavior of form  
+    e.preventDefault();
+
+    // Do something when form is submitted... likely there will be a fetch or axios call here
+    console.log(form);
+  }
+
   return (
     <div className="App">
-      <FormComponent>
+
+      <FormComponent onSubmit={formSubmit}>
 
         <InputComponent
+          type="email"
           label="Email"
           name="email"
+          id="email"
+          required={false}
           value={form.email}
-          onChange={handleFormChange} />
+          onChange={handleInputChange} />
 
         <InputComponent
+          type="password"
           label="Password"
           name="password"
           id="password"
+          required={true}
           value={form.password}
-          onChange={handleFormChange} />
+          onChange={handleInputChange} />
+
+        <ButtonComponent value="Submit"/>
       </FormComponent>
 
     </div>
